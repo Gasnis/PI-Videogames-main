ad0889a1785860ecf52fa69c9749eed4ae5d4d95
Gasnis <gastonsaravia112@gmail.com>
2023-01-03 19:54:19 +0100

    deploy

--- api/index.js
+++ api/index.js
@@ -1,10 +1,11 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 require("dotenv").config()
+const { PORT } = process.env
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(process.env.PORT, () => {
-    console.log('%s listening at ',process.env.PORT); // eslint-disable-line no-console
+  server.listen(PORT, () => {
+    console.log('%s listening at ', PORT); // eslint-disable-line no-console
   });
 });
